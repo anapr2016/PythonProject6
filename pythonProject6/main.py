0f738a8e098a71ede4593059fed27532a3d59ab6
anapr2016 <an.apr2016@yandex.ru>
2024-09-08 20:51:07 +0300

    Add files via upload

---
+++ pythonProject6/main.py
@@ -0,0 +1,7 @@
+print(12+89-1)# This is a sample Python script.
+print('1 st program',(9**0.5)*5)
+print('2hd program', 9.99 > 9.98 and 1000!=1000.1)
+print('3rd program', (1234%1000//10) + (5678%1000//10))
+print('4th program', int(13.42) == int(42.13*100%100))
+
+
